558e9fc0292bed5c9ce13cf2b489726c7f830790
Vladimir <ivanovvladimirsergeevich@gmail.com>
2020-01-13 14:44:26 +0300

    Add files via upload

--- ConteinersTest/test.cpp
+++ ConteinersTest/test.cpp
@@ -1,10 +1,10 @@
 #include "pch.h"
-#include "../Containers/array.h"
+#include "../Containers/Array.h"
 
 using namespace std;
 
 int* toArray(Array<int> a) {
-    int const size = a.getSize();
+    int const size = a.size();
     int* arr;
     arr = new int[size];
     for (int i = 0; i < size; ++i)
@@ -23,7 +23,7 @@ TEST(ContainersTest, Test) {
     delete toArr;
 
     a.remove(100);
-    int arr1[] = {200};
+    int arr1[] = { 200 };
     toArr = toArray(a);
     EXPECT_TRUE(0 == std::memcmp(arr1, toArr, sizeof(arr1)));
     delete toArr;
@@ -32,16 +32,16 @@ TEST(ContainersTest, Test) {
     for (i = 0; i < 10; i++)
         a.insert(i);
 
-    for (i = 0; i < a.getSize(); ++i)
+    for (i = 0; i < a.size(); ++i)
         a[i] *= 2;
 
-    int arr2[] = {400, 0, 2, 4, 6, 8, 10, 12, 14, 16};
+    int arr2[] = { 400, 0, 2, 4, 6, 8, 10, 12, 14, 16 };
     toArr = toArray(a);
-	EXPECT_TRUE(0 == std::memcmp(arr2, toArr , sizeof(arr2)));
+    EXPECT_TRUE(0 == std::memcmp(arr2, toArr, sizeof(arr2)));
     delete toArr;
 
     a[1] = 100;
-    int arr3[] = {400, 100, 2, 4, 6, 8, 10, 12, 14, 16 };
+    int arr3[] = { 400, 100, 2, 4, 6, 8, 10, 12, 14, 16 };
     toArr = toArray(a);
     EXPECT_TRUE(0 == std::memcmp(arr3, toArr, sizeof(arr3)));
     delete toArr;
